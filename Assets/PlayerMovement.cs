05b5bb325e731e9f9e3dbc18e17a320e6c90712c
Sergio Perezalonso <s0552792@htw-berlin.de>
2017-12-12 20:30:50 +0100

    Unity project setup

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //script to move the player in the scene exactly as you would move in an FPS game
+
+    float distance = 5f;
+    public float speed = 40.0f;
+    GameObject hmdCam;
+
+    void Awake()
+    {
+        hmdCam = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;       // Keyboard input
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        // Follow the Vive cam so that the headset rig moves forward in the direction the Vive is facing
+        transform.position += (hmdCam.transform.right * x + hmdCam.transform.forward * z) * speed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);       // This is done so that the player stays on the ground
+    }
+}
---
+++ Assets/drawAnimController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawAnimController : MonoBehaviour
+{
+
+    public Animator anim;
+    bool isOpen = false;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0") && !isOpen)
+        {
+            anim.Play("drawerAnimation");
+            isOpen = true;
+        }
+        else if (Input.GetKeyDown("joystick button 0") && isOpen)
+        {
+            anim.Play("drawerAnimationReverse");
+            isOpen = false;
+        }
+    }
+}
