ec65833c3326c2efe81cec2c4cc22a1970549ca2
“zany12” <zainabtariq12@gmail.com>
2018-01-31 21:11:13 +0100

    exit screen added

---
+++ Assets/UIToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggle : MonoBehaviour {
+
+    public GameObject inventoryPanel;
+    bool panelVisibility = false;
+	// Use this for initialization
+	void Start () {
+        inventoryPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("k")){
+            Debug.Log("Working");
+        }
+        
+        if(Input.GetKeyDown("k") && !panelVisibility){
+            inventoryPanel.SetActive(true);
+            panelVisibility = true;
+        }
+		else if (Input.GetKeyDown("k") && panelVisibility)
+		{
+            inventoryPanel.SetActive(false);
+            panelVisibility = false;
+		}
+		
+	}
+}
