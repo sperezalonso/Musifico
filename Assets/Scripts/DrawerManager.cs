7902af79cc2f0708e6473dfb48164d47d3497d50
“zany12” <zainabtariq12@gmail.com>
2018-02-08 12:18:59 +0100

    inventory panel + sprites + visibility functionality added

--- Assets/Scripts/DrawerManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DrawerManager : MonoBehaviour {
-
-	public enum Drawers {drawerR1, drawerR2, drawerR3, drawerL1, drawerL2, drawerL3}
-	public Drawers whatDrawerAmI;
-}
--- Assets/Scripts/UiToggle.cs
+++ Assets/Scripts/UiToggle.cs
@@ -6,29 +6,90 @@ public class UiToggle : MonoBehaviour
 {
     public GameObject inventoryPanel;
     bool panelVisibility = false;
-
+    GameObject[] tapes; 
+    GameObject caughtTapes; 
+    public GameObject[] icons;
     // Use this for initialization
     void Start()
     {
-        inventoryPanel.SetActive(false);
+        //inventoryPanel.SetActive(false);
+        tapes = GameObject.FindGameObjectsWithTag("tape");
+        caughtTapes = GameObject.Find("Caught Tapes");
+
+        inventoryPanel.GetComponent<CanvasRenderer>().SetAlpha(0f); //set the opacity of inventory panels to 0 
+        icons = GameObject.FindGameObjectsWithTag("UI");
+
+        //hide all inventory icons
+        for (int i = 0; i < icons.Length; i++){
+            icons[i].GetComponent<CanvasRenderer>().SetAlpha(0.5f); //set opacity of icons to 0.5 
+        }
     }
 
-   // Update is called once per frame
-   void Update()
-	{
-        if (Input.GetKeyDown("k") || Input.GetKeyDown("joystick button 2"))
-		{
-			Debug.Log("Working");
-        }
-		if (Input.GetKeyDown("k") && !panelVisibility)
-		{
-			inventoryPanel.SetActive(true);
+    // Update is called once per frame
+    void Update()
+    {
+        //Toggle inventory panel 
+
+        if ((Input.GetKeyDown("k") || Input.GetKeyDown("joystick button 2")) && !panelVisibility)
+        {
+        //inventoryPanel.SetActive(true);
+            inventoryPanel.GetComponent<CanvasRenderer>().SetAlpha(1f);
 			panelVisibility = true;
         }
-        else if (Input.GetKeyDown("k") && panelVisibility)
+        else if ((Input.GetKeyDown("k") || Input.GetKeyDown("joystick button 2")) && panelVisibility)
         {
-            inventoryPanel.SetActive(false);
+            Debug.Log("inventory is visible now");
+            //inventoryPanel.SetActive(false);
+            inventoryPanel.GetComponent<CanvasRenderer>().SetAlpha(0f);
             panelVisibility = false;
         }
+
+        //Set opacity of icons to 100% depending on the collected Cassettes 
+        for (int i = 0; i < tapes.Length; i++)
+        {
+            if (tapes[i].transform.parent == caughtTapes.transform)
+            {
+                Debug.Log("tape found");
+        
+                switch (tapes[i].name){
+                    case "cassette1":
+                        showCassetteIcon(1);
+                    break;
+				case "cassette2":
+						showCassetteIcon(2);
+						break;
+				case "cassette3":
+						showCassetteIcon(3);
+						break;
+				case "cassette4":
+						showCassetteIcon(4);
+						break;
+				case "cassette5":
+						showCassetteIcon(5);
+						break;
+				case "cassette6":
+						showCassetteIcon(6);
+						break;
+				case "cassette7":
+						showCassetteIcon(7); 
+						break;
+
+                default:
+                        Debug.Log("no cassette found");
+			        break;
+            }
+
+            }
+        }
     }
+
+    void showCassetteIcon(int i){
+        GameObject.Find("TapeIcon" + i).GetComponent<CanvasRenderer>().SetAlpha(1f);
+    }
+
+    public void playCassette(int i){
+        GameObject.Find("cassette" + i).GetComponent<AudioSource>().Play();
+    }
+  
+
 }
